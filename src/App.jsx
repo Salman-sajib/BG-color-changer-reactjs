65bf51bfc8da621f1768cc34cd3e28731f732593
Salman-sajib <sfsajib@gmail.com>
2024-02-05 14:01:22 +0600

    instead of changeing the color by using a function change it by calling the setter state directly on the jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { useState } from 'react';
 function App() {
   const [color, setColor] = useState('olive');
 
-  function changeColor(color) {
-    setColor(color);
-  }
+  // function changeColor(color) {
+  //   setColor(color);
+  // }
 
   return (
     <div
@@ -15,19 +15,19 @@ function App() {
       <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
         <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
           <button
-            onClick={() => changeColor('red')}
+            onClick={() => setColor('red')}
             className='outline-none px-4 py-1 rounded-full shadow-lg text-black'
           >
             Red
           </button>
           <button
-            onClick={() => changeColor('green')}
+            onClick={() => setColor('green')}
             className='outline-none px-4 py-1 rounded-full shadow-lg text-black'
           >
             Green
           </button>
           <button
-            onClick={() => changeColor('blue')}
+            onClick={() => setColor('blue')}
             className='outline-none px-4 py-1 rounded-full shadow-lg text-black'
           >
             Blue
